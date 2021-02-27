0ac588323e092667a4b77fd12bfae97681cc3801
Nikolay Yankin <nikolay@rockstonedev.com@users.noreply.github.com>
2021-02-27 19:50:56 +0500

    Updated test.php

--- test.php
+++ test.php
@@ -6,29 +6,33 @@
 
 	$ch = new ClickHouse(CLICKHOUSE_HOST, CLICKHOUSE_USER, CLICKHOUSE_PASSWORD, CLICKHOUSE_DATABASE, CLICKHOUSE_PORT);
 
- 	$result = $ch->query("SELECT
- 		toUInt32(1),
-  		toUInt32(-1),
-  		toInt32(-1),
+	$result = $ch->query("SELECT
+		toUInt8(1),
+		toUInt8(-1),
+		toUInt16(1),
+		toUInt16(-1),
+		toUInt32(1),
+		toUInt32(-1),
+		toUInt64(1),
+		toUInt64(-1),
 		toUInt64(18446744073709551615),
- 		toUInt64(9223372036854775807),
- 		toFloat64(1 / 3),
- 		toFloat32(1 / 3),
- 		toFixedString('test', 8),
- 		toString('test'),
- 		1 == 1,
- 		NULL,
+		toUInt64(9223372036854775807),
+		toFloat64(1 / 3),
+		toFloat32(1 / 3),
+		toFixedString('test', 8),
+		toString('test'),
+		toDate('2021-01-01'),
+		toDateTime('2021-01-01 00:00:00'),
 		toInt8OrNull('123'),
-		toInt8OrNull('123qwe123')
- 	") or trigger_error("Failed to query: ".$ch->error." (".$ch->errno.")", E_USER_ERROR);
+		toInt8OrNull('123qwe123'),
+		1 == 1,
+		NULL
+	") or trigger_error("Failed to run query: ".$ch->error." (".$ch->errno.")", E_USER_ERROR);
 
-	$total = 0;
- 	while ($row = $result->fetch_assoc())
- 	{
- 		$total++;
+	while ($row = $result->fetch_assoc())
+	{
 		print_r($row);
 		var_dump($row);
-//		exit;
 	}
 
 	echo "Total: ".$total."\n";
